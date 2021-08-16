2d3a811a3dabb5ac8ece80d674bedf13485cc151
CamMeiners <cammeiners@gmail.com>
2021-08-16 15:41:50 -0600

    finished the project

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+      counter2 has a second function that encompasses all of it, so the whole thing can be invoked
   2. Which of the two uses a closure? How can you tell?
-  
+      Counter 2, counter is reaching into counterMaker and grabbing count
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random()*Math.floor(3));
 }
 
 
@@ -81,18 +81,36 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
-}
+function finalScore(inningcb, inningNumber){
+   let homeScore = 0;
+   let awayScore = 0;
+   const gameScore = {};
+   for (let i =0; i >inningNumber; i++){
+     const currentScore = inningcb(inningNumber);
+     homeScore = homeScore + inningcb(),
+     awayScore = awayScore + inningcb()
+   }
+   gameScore.Home = homeScore
+   gameScore.Away = awayScore
+   return gameScore
+ }
+ console.log(finalScore(inning, 9))
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningcb) {
+  const gameScore={}
+  let homeScore = inningcb()
+  let awayScore = inningcb()
+  gameScore.Home = homeScore
+  gameScore.Away = awayScore
+  return gameScore
 }
+console.log(getInningScore(inning));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
